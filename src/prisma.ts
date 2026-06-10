e640567b4c45f354c51c809577babba307257bda
nitinn13 <nitinjha080@gmail.com>
2026-06-11 02:24:31 +0530

    fix prisma esm import for azure

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+
+const { PrismaClient } = pkg;
+
+console.log("Prisma loaded successfully");
 
 export const prisma = new PrismaClient();
